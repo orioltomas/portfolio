c0b34a490d8771623da550d7330b41c3f9fb1d6d
orioltomas <oriol@uritomas.local>
2019-10-02 08:09:11 +0200

    Navbar changed from left to right

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,6 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    .addEntry('styles', './assets/css/styles.css')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
