92309e5eb42295b56717bebc74993b3b30788076
orioltomas <oriol@tomasfortuny.com>
2020-12-13 01:03:48 +0100

    Fixed hover and click events on filters

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -14,31 +14,38 @@ export default class Portfolio {
     registerEvents() {
         document.querySelectorAll('.portfolio__filter').forEach((element) => {
             element.addEventListener('click', (e) => this.handleFilterClick(e));
+            element.addEventListener('mouseenter', (e) => this.handleFilterHoverEnter(e));
+            element.addEventListener('mouseleave', (e) => this.handleFilterHoverLeave(e));
         });
+    }
+
+    handleFilterHoverEnter(e) {
+        e.preventDefault();
+        e.currentTarget.classList.add('hover');
+    }
+
+    handleFilterHoverLeave(e) {
+        e.preventDefault();
+        e.currentTarget.classList.remove('hover');
     }
 
     handleFilterClick(e) {
         e.preventDefault();
-
         e.currentTarget.classList.toggle('selected');
+        e.currentTarget.classList.remove('hover');
         this.updateFilteredProjects();
     }
 
     updateFilteredProjects() {
-
         let filters = [];
         document.querySelectorAll('.portfolio__filter.selected').forEach((element) => {
             filters.push([element.getAttribute('data-filter')]);
         });
 
         const filtersString = filters.length > 0 ?  '.' + filters.join('.') : '';
-
         document.querySelectorAll(".portfolio__overview-project").forEach((element) => {
             element.classList.add('hidden');
         });
-
-        console.log(document.querySelectorAll(".portfolio__overview-project" + filtersString));
-
         document.querySelectorAll(".portfolio__overview-project" + filtersString).forEach((element) => {
             element.classList.remove('hidden');
         });
